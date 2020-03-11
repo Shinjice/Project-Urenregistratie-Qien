e019005ece9914ece0f309ca6800b5b97e7a52dc
Shinjice <tdna84@gmail.com>
2020-03-11 09:24:51 +0100

    first

---
+++ src/main/java/nl/qien/uren/urenregistratie/UrenregistratieApplication.java
@@ -0,0 +1,13 @@
+package nl.qien.uren.urenregistratie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrenregistratieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrenregistratieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/qien/uren/urenregistratie/UrenregistratieApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.qien.uren.urenregistratie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrenregistratieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
