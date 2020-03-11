ae919d76b85985ee2a630753df0730437b8e9adb
Shinjice <tdna84@gmail.com>
2020-03-11 17:08:45 +0100

    getMap

--- src/main/java/nl/qien/uren/urenregistratie/domein/Persoon.java
+++ src/main/java/nl/qien/uren/urenregistratie/domein/Persoon.java
@@ -11,6 +11,22 @@ public class Persoon {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
-	private String name;
+	private String naam;
+	
+
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return naam;
+	}
+	public void setName(String naam) {
+		this.naam = naam;
+	}
 	
 }
--- src/main/java/nl/qien/uren/urenregistratie/service/PersoonService.java
+++ src/main/java/nl/qien/uren/urenregistratie/service/PersoonService.java
@@ -4,13 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import nl.qien.uren.urenregistratie.domein.Persoon;
 import nl.qien.uren.urenregistratie.repository.PersoonRepository;
+
 
 @Service
 @Transactional
 public class PersoonService {
 	@Autowired
-	PersoonRepository d;
+	PersoonRepository persoonRepository;
 	
+	public void addPersoon(Persoon persoon) {
+		persoonRepository.save(persoon);
+	}
+
+	public Iterable <Persoon> findAll(){
+		Iterable <Persoon> result = persoonRepository.findAll();
+		return result;
+	}
 	
 }
