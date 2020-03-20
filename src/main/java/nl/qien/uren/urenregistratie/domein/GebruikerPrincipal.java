d2323b0079e3c6755f9d579195cac31a9a7a3448
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2020-03-20 16:50:22 +0100

    Remove unecessary import

--- src/main/java/nl/qien/uren/urenregistratie/domein/GebruikerPrincipal.java
+++ src/main/java/nl/qien/uren/urenregistratie/domein/GebruikerPrincipal.java
@@ -1,6 +1,5 @@
 package nl.qien.uren.urenregistratie.domein;
 
-import org.springframework.context.annotation.Role;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
