c2bf12cd463ec81eab201897d35e7a6c568d1ba9
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2020-03-20 12:45:53 +0100

    Rename branch to featureSecurity

--- src/main/java/nl/qien/uren/urenregistratie/ApplicationSecurity.java
+++ src/main/java/nl/qien/uren/urenregistratie/ApplicationSecurity.java
@@ -1,5 +1,6 @@
 package nl.qien.uren.urenregistratie;
 
+import nl.qien.uren.urenregistratie.service.GebruikerDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,4 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void initialize(AuthenticationManagerBuilder builder, GebruikerDetailsService gebruikerDetailsService) {
+        try {
+            builder.userDetailsService(gebruikerDetailsService);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
